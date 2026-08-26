4ded77ed9303c82c91053d0b35f785a5ad63021f
Andy Lechlak <AndrewLechlak@gmail.com>
2026-08-26 15:49:59 -0400

    test
    test

--- netlify/functions/sierra-proxy.js
+++ netlify/functions/sierra-proxy.js
@@ -13,7 +13,7 @@ exports.handler = async function(event, context) {
     
     // Check if we are requesting the token endpoint. If so, inject the secret server-side.
     if (path === '/token' && event.httpMethod === 'POST') {
-        const token = process.env.SIERRA_API_TOKEN 
+        const token = process.env.SIERRA_API_TOKEN || "Basic UTZYVmI0THdRSXdGOGppZ1JUUC9hbUlGYnBTTTpvcGl1ZXdxcmtsamhzYWR2bGtqc2Rmb2l1ZXdybjkzMjcrMjEz";
         headers['authorization'] = token;
     }
 
